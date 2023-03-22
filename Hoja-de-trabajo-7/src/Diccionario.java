c92eafb3a91299af1702e67ab01f813480984f29
Andre <Andrewlol774@gmail.com>
2023-03-22 08:45:22 -0600

    Creacion de clases
    Signed-off-by: Andre <Andrewlol774@gmail.com>

--- Hoja-de-trabajo-7/src/Diccionario.java
+++ Hoja-de-trabajo-7/src/Diccionario.java
@@ -1,5 +1,25 @@
+
+
+// Java Program to Print Colored Text in Console
+  
+// Importing input output classes
+import java.io.*;
+  
+// Main class
 public class Diccionario {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+   
+  
+    // Declaring ANSI_RESET so that we can reset the color
+    static String ANSI_RESET = "\u001B[0m";
+  
+
+  
+    // Main driver method
+    public static void main(String[] args){ 
+        // Printing the text on console prior adding
+        // the desired color
+        System.out.println(decoration.RED_BOLD + "" + decoration.RESET);
+
+        
     }
 }
